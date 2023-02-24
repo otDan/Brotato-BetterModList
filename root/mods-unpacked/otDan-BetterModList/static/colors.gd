c7230ba101cb42b227bd60b9729c5790d759750f
otDan <otdanofficial@gmail.com>
2023-02-24 12:12:50 +0100

    Add clickable website url

--- root/mods-unpacked/otDan-BetterModList/static/colors.gd
+++ root/mods-unpacked/otDan-BetterModList/static/colors.gd
@@ -3,4 +3,4 @@ extends Node
 
 var loaded: String = ""
 var unloaded: String = "#c45151"
-
+var interactive: String = "#faee70"
--- root/mods-unpacked/otDan-BetterModList/ui/menus/pages/mods/mod_info_container.gd
+++ root/mods-unpacked/otDan-BetterModList/ui/menus/pages/mods/mod_info_container.gd
@@ -3,6 +3,7 @@ extends PanelContainer
 
 onready var _mod_name = $"%ModName" as Label
 onready var _mod_author = $"%ModAuthor" as Label
+onready var _mod_website = $"%ModWebsite" as RichTextLabel
 onready var _mod_version = $"%ModVersion" as Label
 onready var _mod_description = $"%ModDescription" as RichTextLabel
 onready var _mod_dependency_container = $"%DependecyContainer" as VBoxContainer
@@ -17,6 +18,7 @@ func _ready()->void:
 func set_data(mod:ModData)->void:
 	_mod_name.text = mod.manifest.name
 	_mod_author.text = str(mod.manifest.authors)
+	_mod_website.bbcode_text = "[color=" + Colors.interactive + "][url]" + mod.manifest.website_url + "[/url][/color]"
 	_mod_version.text = mod.manifest.version_number
 	_mod_description.bbcode_text = mod.manifest.description
 	if not PoolStringArray(mod.manifest.dependencies).empty():
@@ -40,7 +42,13 @@ func set_data(mod:ModData)->void:
 func set_empty()->void:
 	_mod_name.text = ""
 	_mod_author.text = ""
+	_mod_website.text = ""
 	_mod_version.text = ""
 	_mod_description.bbcode_text = ""
 	_mod_dependencies.text = ""
 	_mod_dependency_container.visible = false
+
+
+func _on_ModWebsite_meta_clicked(meta):
+	OS.shell_open(meta)
+	pass # Replace with function body.
