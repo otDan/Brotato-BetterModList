b83738a683f0a86c65578bae9a8110b138e02b45
otDan <otdanofficial@gmail.com>
2023-02-27 12:11:30 +0100

    Update mod name look

--- root/mods-unpacked/otDan-BetterModList/ui/menus/pages/mods/components/mod_container.gd
+++ root/mods-unpacked/otDan-BetterModList/ui/menus/pages/mods/components/mod_container.gd
@@ -7,27 +7,24 @@ signal mod_unfocused(mod_data)
 var mod_data:ModData
 var mod_style
 
-onready var _mod_name = $"%ModName" as RichTextLabel
-
 onready var Colors = get_node("/root/ModLoader/otDan-BetterModList/Colors")
+
+onready var mod_name_button: Button = $"%ModNameButton"
+
 
 func _ready():
 	var style = get_stylebox("panel")
 	mod_style = style.duplicate()
 	add_stylebox_override("panel", mod_style)
 
+
 func set_data(p_mod:ModData)->void:
 	mod_data = p_mod
-	var mod_name: String
+	mod_name_button.text = mod_data.manifest.name
 	if mod_data.is_loadable:
 		mod_style.bg_color = Color(Colors.loaded)
-		mod_name += "[color=" + Colors.loaded + "]"
 	else:
 		mod_style.bg_color = Color(Colors.unloaded)
-		mod_name += "[color=" + Colors.unloaded + "]"
-	mod_name += str(mod_data.manifest.name)
-	mod_name += "[/color]"
-	_mod_name.bbcode_text = mod_name
 
 
 func _on_ModName_focus_entered()->void:
