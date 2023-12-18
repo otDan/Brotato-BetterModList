87a8b2296455bd3b4962fc9159bc26c4d73d2012
otDan <otdanofficial@gmail.com>
2023-12-18 09:16:09 +0100

    Refactor: Add new sort to node sorter

--- root/mods-unpacked/otDan-BetterModList/global/node_sorter.gd
+++ root/mods-unpacked/otDan-BetterModList/global/node_sorter.gd
@@ -8,3 +8,16 @@ func sort(a, b):
 
 func reverse_sort(a, b):
 	return a.name > b.name
+
+
+func sort_descending(version1: String, version2: String):
+	var version1_parts = version1.split('.')
+	var version2_parts = version2.split('.')
+
+	for i in range(0, 4):
+		if int(version1_parts[i]) > int(version2_parts[i]):
+			return false
+		elif int(version1_parts[i]) < int(version2_parts[i]):
+			return true
+
+	return false
